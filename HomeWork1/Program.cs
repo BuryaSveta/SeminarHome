5ebde4992c434c661f5c4020f57fe079bac61c85
Svetlana <svetlana.buryakova@mail.ru>
2022-08-31 09:36:06 +0300

    Ready task 2

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+/*
+Console.Write("Input first integen number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integen number: ");
+int num2 = Convert.ToInt32(Console.ReadLine()); 
+
+if(num1 > num2)
+{
+    Console.WriteLine("{0} the largest number ", num1);
+}
+else
+{
+     Console.WriteLine("{0} the largest number ", num2);
+}
+
+*/
+
