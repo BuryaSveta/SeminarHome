7a1cb79103e27c0bacb235b0771375babd9186d8
Svetlana <svetlana.buryakova@mail.ru>
2022-09-11 10:09:30 +0300

    Added homework 5

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,109 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+/*
+int[] numbers = new int[10];
+
+void FillArray(int[] array, int min, int max)
+{
+  for (int i = 0; i<array.Length; i++ )
+  {
+    array[i] = new Random().Next(min, max);
+  }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i<array.Length; i++ )
+    {
+        Console.Write(array[i] + " ");
+    }
+  Console.WriteLine();
+}
+
+int QuantityPositive(int[] array)
+{
+    int quantity = 0;
+    for (int i = 0; i<array.Length; i++ )
+    {
+        if (array[i] % 2 == 1)
+        {
+            quantity++;
+        }
+    }
+    return quantity;
+}
+
+FillArray(numbers, 100, 1000);
+WriteArray(numbers);
+Console.WriteLine();
+
+int quantity = QuantityPositive(numbers);
+Console.WriteLine($"The number of even numbers in the array: {quantity}");
+*/
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+/*
+Console.WriteLine("Enter the size of the array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int sum = 0;
+
+for (int n = 0; n < numbers.Length; n += 2)
+    sum = sum + numbers[n];
+
+    Console.WriteLine($"Sum of elements in odd positions: {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.WriteLine();
+}
+
+*/
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+/*
+double[] arrayRealNumbers = new double[10];
+  for (int i = 0; i < arrayRealNumbers.Length; i++ )
+  {
+    arrayRealNumbers[i] = new Random().Next(1, 10);
+    Console.Write(arrayRealNumbers[i] + " ");
+  }
+
+double maxNumber = arrayRealNumbers[0];
+double minNumber = arrayRealNumbers[0];
+
+  for (int i = 1; i < arrayRealNumbers.Length; i++)
+  {
+    if (maxNumber < arrayRealNumbers[i])
+    {
+      maxNumber = arrayRealNumbers[i];
+    }
+        if (minNumber > arrayRealNumbers[i])
+    {
+      minNumber = arrayRealNumbers[i];
+    }
+  }
+
+  double decision = maxNumber - minNumber;
+
+  Console.WriteLine($"= It turned out that the difference between the maximum ({maxNumber}) and minimal ({minNumber}) is: {decision} ");
+*/ 
